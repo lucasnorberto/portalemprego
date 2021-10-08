298d6489abf93444022c1fb038173bbd0e9b37e9
Lucas Norberto <lucasnorberto@gmail.com>
2021-10-07 23:50:36 -0300

    hom3

---
+++ ppe/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'footer', component: FooterComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
